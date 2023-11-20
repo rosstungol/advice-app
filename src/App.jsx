c83fc9a1bbdd10ed3d50c10d63a64eed6c96e5b4
Ross Tungol <ross@proudcloud.io>
2023-11-20 23:46:15 +0800

    Adds useState, useEffect and fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react"
 import "./App.css"
+import axios from "axios"
 
 const App = () => {
+  const [advice, setAdvice] = useState("")
+  console.log(advice)
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  function fetchAdvice() {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip
+        setAdvice(advice)
+      })
+      .catch((error) => console.log(error))
+  }
+
   return <h1>Hi</h1>
 }
 
